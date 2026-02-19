41510327e2d0ed1bd032ffcfd7f4a78fd621a76d
LudensNoir <nichoarthur17@gmail.com>
2026-02-19 19:07:23 +0700

    Progress verify user

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from 'antd';
+
 export default function Home() {
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-putih font-sans text-black">
       <h4>MEMK</h4>
-      
+      <Button>Log Out</Button>
     </div>
   );
 }
