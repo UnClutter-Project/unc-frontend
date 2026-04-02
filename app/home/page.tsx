5afeefeea9fa51a0dca55b1d92f42d73aa3b67f1
LudensNoir <nichoarthur17@gmail.com>
2026-04-02 14:09:14 +0700

    Add Flowbite

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle  } from "flowbite-react";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -28,6 +30,24 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-putih font-sans text-black">
+      <Navbar fluid rounded>
+      <NavbarBrand as={Link} href="https://flowbite-react.com">
+        <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">UnClutter</span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink as={Link} href="#">
+          About
+        </NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
       <h4>MEMK</h4>
       <button onClick={logout}>LOGOUT</button>
     </div>
